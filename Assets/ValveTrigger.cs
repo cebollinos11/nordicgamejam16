bd9c2d136294e4cbbcd075b6f3121fd83ed2a355
cebollinos11 <cebollinos@gmail.com>
2016-04-09 13:26:55 +0200

    pushed valve

---
+++ Assets/ValveTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValveTrigger : MonoBehaviour {
+
+    
+    public bool wasTriggered;
+    public string name;
+
+    ValveScript vs;
+	// Use this for initialization
+	void Start () {
+        vs = transform.parent.GetComponent<ValveScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("Trigger "+name);
+        wasTriggered = true;
+        vs.CheckTriggers();
+    }
+}

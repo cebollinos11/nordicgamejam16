fe990ef82d64f2514cd389633fa65faca1ef3ba2
Martin <me_thomsen@hotmail.com>
2016-04-09 16:48:35 +0200

    Room

--- Assets/Scripts/AntDirector.cs
+++ Assets/Scripts/AntDirector.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AntDirector : MonoBehaviour {
+public class AntDirector : MonoBehaviour
+{
 
+    [SerializeField] private AnimationCurve difficultyCurve;
+    [SerializeField] private AnimationCurve floodDuration;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,11 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Room
+public class Room : MonoBehaviour
 {
 
     public enum RoomState 
     {
-
+        Filling,
+        Idle
     };
+
+    public RoomState state;
+    public bool isLocked = false;
+    public float filledAmount { get; private set; }
+    public float fillOverflowLimit = 65;
+    public float fillSpeed = 1;
+
+    void Start()
+    {
+        filledAmount = 0;
+        state = RoomState.Idle;
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetLock(bool locked)
+    {
+        isLocked = locked;
+    }
+
+    public void Fill(float amount)
+    {
+        filledAmount += amount;
+    }
 }

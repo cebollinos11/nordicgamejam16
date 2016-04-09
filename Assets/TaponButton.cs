fbdde703a8094cbcdcb6032142d6be47424ac5ab
cebollinos11 <cebollinos@gmail.com>
2016-04-09 16:45:59 +0200

    button class

---
+++ Assets/TaponButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaponButton : Button {
+
+    public override void OnPress()
+    {
+        base.OnPress();
+
+        Debug.Log("TAPON");
+    }
+}

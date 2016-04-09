0bc6a14951c7df5eccde2a615612ad5d382f9559
Martin <me_thomsen@hotmail.com>
2016-04-09 14:04:39 +0200

    Input controller

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+
+public class InputController : MonoBehaviour
+{
+    public InputTester inputTester;
+
+    private GamepadState controller1;
+    private GamepadState controller2;
+    private GamepadState controller3;
+    private GamepadState controller4;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        controller1 = GamepadInput.GamePad.GetState(GamePad.Index.One);
+        controller2 = GamepadInput.GamePad.GetState(GamePad.Index.Two);
+        controller3 = GamepadInput.GamePad.GetState(GamePad.Index.Three);
+        controller4 = GamepadInput.GamePad.GetState(GamePad.Index.Four);
+    }
+
+	void Update ()
+	{
+        Debug.Log("1: " + controller1.LeftStickAxis + "  2: " + controller2.LeftStickAxis +"  3: " + controller3.LeftStickAxis + "  4: " +controller4.LeftStickAxis);
+    }
+}
---
+++ Assets/Scripts/InputTester.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputTester : MonoBehaviour
+{
+
+    public Text N64Text;
+    public Text N64ZText;
+    public Text PS3LStickText;
+    public Text PS3RStickText;
+    public Text TriggerText;
+
+    public void UpdateTest(string n64, string n64z, string ps3lstick, string ps3rstick, string trigger)
+    {
+        N64Text.text = n64;
+        N64ZText.text = n64z;
+        PS3LStickText.text = ps3lstick;
+        PS3RStickText.text = ps3rstick;
+        TriggerText.text = trigger;
+    }
+    
+
+}

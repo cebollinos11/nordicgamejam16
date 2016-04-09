43dc6e211cdd0eee78c1360b8d9bb205d3bef32b
cebollinos11 <cebollinos@gmail.com>
2016-04-09 22:33:49 +0200

    mix

--- Assets/GuacamoleButton.cs
+++ Assets/GuacamoleButton.cs
@@ -5,11 +5,35 @@ public class GuacamoleButton : Button {
 
     public Room roomToFill;
 
+    public bool isLeaking;
+
+
+    public void StartLeaking() {
+        isLeaking = true;
+    }
+
+    public void StopLeaking()
+    {
+        ShaderManager.LayerMask(GetComponent<SpriteRenderer>(), Color.blue);
+        Debug.Log("FUCK IT");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        StartLeaking();
+    }
+
     public override void OnPress()
     {
         base.OnPress();
-        ShaderManager.LayerMask(GetComponent<SpriteRenderer>(), Color.blue);
-        Debug.Log("PRess");
+
+        if (isLeaking)
+        {
+            StopLeaking();
+        }
+        
+        
         
     }
    

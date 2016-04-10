78244cce5d7c1b5c0460698d6786ce9094430a29
cebollinos11 <cebollinos@gmail.com>
2016-04-10 13:12:34 +0200

    counter

---
+++ Assets/Counter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    Text t;
+    int i;
+
+    void Increment() {
+        i++;
+    }
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Text>();
+        i = 0;
+        InvokeRepeating("Increment", 1f, 1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.text = i.ToString();
+	}
+}

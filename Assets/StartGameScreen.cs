5cf921442da934bf07f7fda11722d63d19702882
cebollinos11 <cebollinos@gmail.com>
2016-04-10 10:58:12 +0200

    more scenes

---
+++ Assets/StartGameScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameScreen : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+            Debug.Log("you*******");
+            Application.LoadLevel(1);
+        }
+
+	}
+}
